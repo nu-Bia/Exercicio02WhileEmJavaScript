4842f70ef7a4b10c36875ff70939ce82cbc0c822
Núbia <112019351+nu-Bia@users.noreply.github.com>
2022-11-04 13:04:28 -0300

    Add files via upload

---
+++ b).js
@@ -0,0 +1,12 @@
+
+    function soma() {
+        var contador = 0;
+        var soma = 0;
+        while (contador <= 100) {
+            var soma = soma + contador;
+            var contador = contador + 1;
+            
+        
+        }
+        document.write("A soma dos cem primeiros número é : " + soma );
+    }
